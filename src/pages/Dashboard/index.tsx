dfbba6f580011d58454e59d33164d0125a9d35ec
artur.andrade <artur.andrade@itriad.org.br>
2024-01-03 13:45:48 -0400

    Lidando com carregamento Assíncrono

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,30 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon, HighlightCards, Transactions, Title, TransactionList, LogoutButton } from "./styles";
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList,
+    LogoutButton,
+    LoadContainer
+} from "./styles";
 import { HighlighCard } from "../../components/HighlightCard";
 import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
-import { getBottomSpace } from "react-native-iphone-x-helper";
-import { GestureHandlerRootView, RefreshControl } from "react-native-gesture-handler";
+import { RefreshControl } from "react-native-gesture-handler";
 import TransactionsService from "../../services/transactions.service";
 import TransactionDTO from "../../Interfaces/TransactionDTO";
 import { categories } from "../../utils/categories";
 import { useFocusEffect } from "@react-navigation/native";
+import { ActivityIndicator } from "react-native";
+import { useTheme } from "styled-components";
 
 export interface DataListProps extends TransactionCardProps {
     id: string
@@ -24,20 +41,12 @@ interface HighlightData {
 }
 
 export function Dashboard() {
+    const [isLoading, setIsLoading] = useState(true);
     const [transactions, setTransactions] = useState<DataListProps[]>([]);
-    const [highlightData, setHighlightData] = useState<HighlightData>({
-        entries: {
-            amount: 'R$ 0,00'
-        },
-        expensives: {
-            amount: 'R$ 0,00'
-        },
-        total: {
-            amount: 'R$ 0,00'
-        }
-    } as HighlightData);
+    const [highlightData, setHighlightData] = useState<HighlightData>({} as HighlightData);
     const [refreshing, setRefreshing] = useState(false);
 
+    const theme = useTheme();
 
     const loadTransactions = async () => {
         try {
@@ -48,6 +57,7 @@ export function Dashboard() {
             console.log(error);
         } finally {
             setRefreshing(false);
+            setIsLoading(false);
         }
     }
 
@@ -119,52 +129,61 @@ export function Dashboard() {
 
     return (
         <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo source={{ uri: 'https://revolucaonerd.com/wordpress/wp-content/files/revolucaonerd.com/2023/03/sanji-em-one-piece.jpg' }} />
-                        <User>
-                            <UserGreeting>Olá, </UserGreeting>
-                            <UserName>Artur</UserName>
-                        </User>
-                    </UserInfo>
-                    <LogoutButton>
-                        <Icon name="power" />
-                    </LogoutButton>
-                </UserWrapper>
+            {
+                isLoading ?
+                    <LoadContainer>
+                        <ActivityIndicator color={theme.colors.attention} />
+                    </LoadContainer>
+                    :
+                    <Fragment>
+                        <Header>
+                            <UserWrapper>
+                                <UserInfo>
+                                    <Photo source={{ uri: 'https://revolucaonerd.com/wordpress/wp-content/files/revolucaonerd.com/2023/03/sanji-em-one-piece.jpg' }} />
+                                    <User>
+                                        <UserGreeting>Olá, </UserGreeting>
+                                        <UserName>Artur</UserName>
+                                    </User>
+                                </UserInfo>
+                                <LogoutButton>
+                                    <Icon name="power" />
+                                </LogoutButton>
+                            </UserWrapper>
 
-            </Header>
-            <HighlightCards>
-                <HighlighCard
-                    title="Entrada"
-                    amount={highlightData.entries.amount}
-                    lastTransaction="12321"
-                    type="up"
-                />
-                <HighlighCard
-                    title="Saída"
-                    amount={highlightData.expensives.amount}
-                    lastTransaction="12321"
-                    type="down"
-                />
-                <HighlighCard
-                    title="Total"
-                    amount={highlightData.total.amount}
-                    lastTransaction="12321"
-                    type="total"
-                />
-            </HighlightCards>
-            <Transactions>
-                <Title>
-                    Listagem
-                </Title>
-                <TransactionList
-                    data={transactions}
-                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadTransactions} />}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => <TransactionCard data={item} />}
-                />
-            </Transactions>
+                        </Header>
+                        <HighlightCards>
+                            <HighlighCard
+                                title="Entrada"
+                                amount={highlightData.entries.amount}
+                                lastTransaction="12321"
+                                type="up"
+                            />
+                            <HighlighCard
+                                title="Saída"
+                                amount={highlightData.expensives.amount}
+                                lastTransaction="12321"
+                                type="down"
+                            />
+                            <HighlighCard
+                                title="Total"
+                                amount={highlightData.total.amount}
+                                lastTransaction="12321"
+                                type="total"
+                            />
+                        </HighlightCards>
+                        <Transactions>
+                            <Title>
+                                Listagem
+                            </Title>
+                            <TransactionList
+                                data={transactions}
+                                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadTransactions} />}
+                                keyExtractor={item => item.id}
+                                renderItem={({ item }) => <TransactionCard data={item} />}
+                            />
+                        </Transactions>
+                    </Fragment>
+            }
         </Container>
     )
 }
